564ed49bcb918c291a45681546452b83a057298e
Lucky Soni <lp.soni@yahoo.co.in>
2022-01-26 15:11:37 +0530

    update icons

--- src/wrapper/index.tsx
+++ src/wrapper/index.tsx
@@ -1,7 +1,7 @@
-import {iosClock} from 'react-icons-kit/ionicons/iosClock'
-import {Icon} from 'react-icons-kit'
-import {arrowMove} from 'react-icons-kit/ionicons/arrowMove'
 import {ReactNode} from 'react'
+import {Icon} from 'react-icons-kit'
+import {ic_access_time_twotone as clockIcon} from 'react-icons-kit/md/ic_access_time_twotone'
+import {ic_drag_indicator_twotone as moveIcon} from 'react-icons-kit/md/ic_drag_indicator_twotone'
 import {Flex} from '../flex'
 import './wrapper.css'
 
@@ -10,10 +10,9 @@ export function Wrapper({children}: {children: ReactNode}) {
     <div className="App">
       <div className="DragAnchor">
         <Flex alignItems="center">
-          <Icon icon={iosClock} className="AppIcon" />{' '}
-          <h1 className="AppTitle">Time Keeper</h1>
+          <Icon icon={clockIcon} className="AppIcon" /> <h1 className="AppTitle">Time Keeper</h1>
         </Flex>
-        <Icon icon={arrowMove} />
+        <Icon icon={moveIcon} />
       </div>
       {children}
     </div>
